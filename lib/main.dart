18da844e9d5f35c692aacc7a5ed70d7c527af92e
Ahmed Farouk <ahmedmohamedfarouk212103048@gmail.com>
2026-03-24 08:14:45 +0200

    flutter_stripe_setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:checkout_payment_ui/Features/checkout/presentation/views/my_cart_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() {
+  Stripe.publishableKey =
+      'pk_test_51TEHjz17vdk8k0iAihxLUB63yLosPa37urS7XHsMut8Iu2ZAbYdRMBIkB83nUSPd99ghCOm1sYdjBF6Qd1p8nWqZ00weV0YyED';
   runApp(const CheckoutApp());
 }
 
